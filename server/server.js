180bbd41a17f703663b3ec1300d87fecdc7a9066
Johnny Goth <Jgoth23@gmail.com>
2021-02-06 12:21:37 -0600

    fixed server problem

--- server/server.js
+++ server/server.js
@@ -1,41 +1,14 @@
 const express = require('express');
 console.log('express', express);
-const guesses = require('./modules/guesses');
 
 // Create our app
 const app = express();
-const PORT = 5000;
 
-// Share any files inside the 'public' folder
-app.use(express.static('server/public'));
-// INCANTATION
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const port = 5000;
 
-app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
-});
 
-// GET & POST Routes go here
-
-// GET route to display previous guesses
-app.get('/guesses', function (request, response) {
-  console.log('getting guesses');
-  console.log(guesses);
-  console.log(guess);
-  response.send(guess);
-});
-
-// POST route to submit guesses
-app.post('/guesses', function (request, response) {
-  console.log('submitted to guesses');
-  let users = request.body.data_to_add;
-  console.log(users);
-  response.sendStatus(200);
-});
-
-// POST route to grab randomInt
-app.post('/get-random', function (request, response) {
-  let guess = String(guesses());
-  response.send(guess);
+// Listen for network requests
+app.listen(port, function () {
+  // When the server is ready, call this function
+  console.log('listening', port);
 });
