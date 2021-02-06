5b846f7ad6ae8d09d314fbdedd5dd92881dc35f0
Johnny Goth <Jgoth23@gmail.com>
2021-02-05 20:39:00 -0600

    server pulled in

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const guesses = require('./modules/guesses');
+
+// Create our app
+const app = express();
+const PORT = 5000;
+
+// Share any files inside the 'public' folder
+app.use(express.static('server/public'));
+// INCANTATION
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
+
+// GET & POST Routes go here
+
+// GET route to display previous guesses
+app.get('/guesses', function (request, response) {
+  console.log('getting guesses');
+  console.log(guesses);
+  console.log(guess);
+  response.send(guess);
+});
+
+// POST route to submit guesses
+app.post('/guesses', function (request, response) {
+  console.log('submitted to guesses');
+  let users = request.body.data_to_add;
+  console.log(users);
+  response.sendStatus(200);
+});
+
+// POST route to grab randomInt
+app.post('/get-random', function (request, response) {
+  let guess = String(guesses());
+  response.send(guess);
+});
