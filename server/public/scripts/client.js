e3f02d0c5b3b89eb76b926367d0afe1768005fb3
Johnny Goth <Jgoth23@gmail.com>
2021-02-07 14:14:27 -0600

    switch working and math functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,114 @@
-console.log('in client.js');
+$(document).ready(readyNow);
 
-$(document).ready(onReady);
-
-function onReady() {
-  console.log('JQ so ready');
+function readyNow() {
+  console.log('jQuery loaded');
+  clickHandlers();
 }
 
+function clickHandlers() {
+  $('#addtionBtn').on('click', additionBtn);
+  $('#subtractBtn').on('click', subtractBtn);
+  $('#multiplyBtn').on('click', multiplyBtn);
+  $('#divideBtn').on('click', divideBtn);
+  $('#clearBtn').on('click', newExperience);
+}
+
+function additionBtn() {
+  let firstValue = $('#input1').val();
+  let secondValue = $('#input2').val();
+  let objectInfo = {
+    firstNumber: firstValue,
+    secondNumber: secondValue,
+    type: 'Add',
+  };
+  console.log('button clicked!');
+  $.ajax({
+    type: 'POST',
+    data: objectInfo,
+    url: '/calculator',
+  }).done(function () {
+    console.log('working!');
+    getCalcLogic();
+  });
+}
+function subtractBtn() {
+  let firstValue = $('#input1').val();
+  let secondValue = $('#input2').val();
+  let objectInfo = {
+    firstNumber: firstValue,
+    secondNumber: secondValue,
+    type: 'Subtract',
+  };
+  console.log('button clicked!');
+  $.ajax({
+    type: 'POST',
+    data: objectInfo,
+    url: '/calculator',
+  }).done(function () {
+    console.log('success!');
+    getCalcLogic();
+  });
+}
+function multiplyBtn() {
+  let firstValue = $('#input1').val();
+  let secondValue = $('#input2').val();
+  let objectInfo = {
+    firstNumber: firstValue,
+    secondNumber: secondValue,
+    type: 'Multiply',
+  };
+  console.log('button clicked!');
+  $.ajax({
+    type: 'POST',
+    data: objectInfo,
+    url: '/calculator',
+  }).done(function () {
+    console.log('working!');
+    getCalcLogic();
+  });
+}
+function divideBtn() {
+  let firstValue = $('#input1').val();
+  let secondValue = $('#input2').val();
+  let objectInfo = {
+    firstNumber: firstValue,
+    secondNumber: secondValue,
+    type: 'Divide',
+  };
+  console.log('button clicked!');
+  $.ajax({
+    type: 'POST',
+    data: objectInfo,
+    url: '/calculator',
+  }).done(function () {
+    console.log('success!');
+    getCalcLogic();
+  });
+}
+
+function getCalcLogic() {
+  $.ajax({
+    type: 'GET',
+    url: '/calculator',
+  }).done(function (response) {
+    appendtoDom(response);
+    console.log('response', response);
+  });
+  newExperience();
+}
+
+function appendtoDom(calculationArr) {
+  console.log('append on');
+
+  let newestEntry = calculationArr.length - 1;
+  let tr = $('<tr></tr>');
+  tr.append('<td>' + calculationArr[newestEntry] + '</td>');
+  $('#equationHistory').append(tr);
+  console.log(calculationArr);
+}
+function newExperience() {
+  console.log('clearBtn Clicked');
+  //$('#calculatorForm').empty();
+  $('#input1').val('');
+  $('#input2').val('');
+}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,53 @@
-const { urlencoded } = require('body-parser');
-const express = require('express');
-console.log('express', express);
+let express = require('express');
+let app = express();
+const PORT = 5000;
+let bodyParser = require('body-parser');
+let calcAnswer = [];
 
-// Create our app
-const app = express();
-
-const port = 5000;
-
-// INCANTATION:
-// Share any files in "./server/public" folder
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('server/public'));
-app.use(express.json());
-app.use(urlencoded({extended: true}))
 
-
-// Listen for network requests
-app.listen(port, function () {
-  // When the server is ready, call this function
-  console.log('listening', port);
+app.listen(PORT, function () {
+    console.log('listening on port', PORT);
 });
+
+app.post('/calculator', (req, res) => {
+    let objectInfo = req.body;
+    let valueX = parseInt(objectInfo.firstNumber);
+    let valueY = parseInt(objectInfo.secondNumber);
+    let mathOperator = objectInfo.type;
+    console.log(objectInfo, valueX, valueY, mathOperator);
+
+    switch(mathOperator) {
+        case "Add":
+        let sumAdd = valueX + valueY;
+        history = valueX + '+' + valueY + '='  + sumAdd;
+        calcAnswer.push(history);
+        console.log(calcAnswer);
+        console.log(sumAdd);
+        break;
+        case "Subtract":
+        let differenceOf = valueX - valueY;
+        history = valueX + '-' + valueY + '='  + differenceOf;
+        calcAnswer.push(history);
+        console.log(differenceOf);
+        break;
+        case "Multiply":
+        let multiplyEnd = valueX * valueY;
+        history = valueX + '*' + valueY + '='  + multiplyEnd;
+        calcAnswer.push(history);
+        console.log(multiplyEnd);
+        break;
+        case "Divide":
+        let divideEnd = valueX / valueY;
+        history = valueX + '/' + valueY + '='  + divideEnd;
+        calcAnswer.push(history);
+        console.log(divideEnd);
+    }
+    res.sendStatus(200);
+})
+
+app.get('/calculator', (req, res) => {
+    res.send(calcAnswer);
+})
